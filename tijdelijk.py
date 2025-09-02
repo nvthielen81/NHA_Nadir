086c1f23018f75b83e6efeaec722c9c5f6a2aca2
nadirvanthielen <nadirvanthielen@gmail.com>
2025-09-02 08:23:29 +0200

    de vligthuig banner sta er nu bij

--- tijdelijk.py
+++ tijdelijk.py
@@ -5,3 +5,5 @@ totaal_prijs = int(string_tmp)
 totaal_prijs = totaal_prijs * aanbieding
 print("Vandaag in de aanbieding: " + smaken_en_prijzen[0:6] + "ijs, 1 liter - slechts Euro " + str(totaal_prijs) + "!")
 
+reclame_text2 = "Sportvliegtuig".upper()
+print(reclame_text2)
